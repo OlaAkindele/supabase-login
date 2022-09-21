8bc88af385251f876300a2f1d5ca6e4d5ac45e60
OlaAkindele <akindele.ok@gmail.com>
2022-09-21 10:37:22 +0100

    worked on form inputs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
+      <header className="App-header">
       </header>
       <b className='logo'>SmartMove</b>
 
       <div className='modal'>
+        <br/>
+        <div>
+        <b>SIGN UP</b>
 
+        </div>
+        <input className="inputs" />
+        <input className="inputs" />
+        <input className="inputs" />
       </div>
     </div>
   );
